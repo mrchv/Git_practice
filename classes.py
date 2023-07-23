6b4d072def205c32027544aed3c089c79efd2259
Dmitry Marichev <mrchv@ya.ru>
2023-07-23 19:50:00 +0100

    Added some Class

--- classes.py
+++ classes.py
@@ -1,1 +1,10 @@
-def
+class QA:
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+
+    def test(self):
+        return "I'm testing!"
+
+tester = QA('Dima', 'Autotesting')
+print(tester.test())
